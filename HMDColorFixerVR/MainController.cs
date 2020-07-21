50f120d0d86e36431493e91a97b53b9fd7c968cd
Andreas Aronsson <BOLL@7708.net>
2020-07-21 20:13:26 +0200

    Add OpenVR, main controller with functionality

---
+++ HMDColorFixerVR/MainController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using BOLL7708;
+using Valve.VR;
+
+namespace HMDColorFixerVR
+{
+    class MainController
+    {
+        public enum Color
+        {
+            R,
+            G,
+            B
+        }
+
+        EasyOpenVRSingleton _vr = EasyOpenVRSingleton.Instance;
+        ConcurrentDictionary<Color, float> _values = new ConcurrentDictionary<Color, float>();
+        volatile bool _changed = false;
+
+        public MainController()
+        {
+            _vr.Init();
+            InitWorkerThread();
+        }
+
+        public void SetColorValue(Color color, float value)
+        {
+            _values[color] = value;
+            _changed = true;
+        }
+
+        private Thread _workerThread = null;
+        public void InitWorkerThread()
+        {
+            _workerThread = new Thread(Worker);
+            if (!_workerThread.IsAlive) _workerThread.Start();
+        }
+
+        private void Worker()
+        {
+            Thread.CurrentThread.IsBackground = true;
+            while (true)
+            {
+                Thread.Sleep(100);
+                if (_vr.IsInitialized() && _changed)
+                {
+                    _changed = false;
+                    if (_values.ContainsKey(Color.R)) _vr.SetFloatSetting(OpenVR.k_pch_SteamVR_Section, OpenVR.k_pch_SteamVR_HmdDisplayColorGainR_Float, _values[Color.R]);
+                    if (_values.ContainsKey(Color.G)) _vr.SetFloatSetting(OpenVR.k_pch_SteamVR_Section, OpenVR.k_pch_SteamVR_HmdDisplayColorGainG_Float, _values[Color.G]);
+                    if (_values.ContainsKey(Color.B)) _vr.SetFloatSetting(OpenVR.k_pch_SteamVR_Section, OpenVR.k_pch_SteamVR_HmdDisplayColorGainB_Float, _values[Color.B]);
+                }
+            }
+        }
+    }
+}
--- HMDColorFixerVR/MainWindow.xaml.cs
+++ HMDColorFixerVR/MainWindow.xaml.cs
@@ -21,19 +21,26 @@ namespace HMDColorFixerVR
     /// </summary>
     public partial class MainWindow : Window
     {
+        MainController _controller = new MainController();
         public MainWindow()
         {
             InitializeComponent();
+            Title = Properties.Resources.AppName;
         }
 
         private void SliderR_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-
+            _controller.SetColorValue(MainController.Color.R, (float)e.NewValue / 100);
         }
 
-        private void SliderR_MouseUp(object sender, MouseButtonEventArgs e)
+        private void SliderG_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            Debug.WriteLine("TEST");
+            _controller.SetColorValue(MainController.Color.G, (float)e.NewValue / 100);
+        }
+
+        private void SliderB_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            _controller.SetColorValue(MainController.Color.B, (float)e.NewValue / 100);
         }
     }
 }
